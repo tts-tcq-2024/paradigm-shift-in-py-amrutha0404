70a07ac6a10fa5b64938a392c9541a4ffe5dc525
amrutha0404 <99201771+amrutha0404@users.noreply.github.com>
2024-07-25 15:06:31 +0530

    Updated check_limits.py

--- check_limits.py
+++ check_limits.py
@@ -1,59 +1,36 @@
 class BatteryMonitor:
     def __init__(self):
-        self.warning_config = {
-            'temperature': True,
-            'soc': True,
-            'charge_rate': True,
-        }
+        self.warning_config = {'temperature': True, 'soc': True, 'charge_rate': True}
 
     def is_within_range(self, value, min_val, max_val):
-        return min_val <= value <= max_val
+        return min_val <= value <= max_val if min_val is not None else value <= max_val
 
     def is_approaching_limit(self, value, min_val, max_val, tolerance):
-        return (min_val <= value < min_val + tolerance) or (max_val - tolerance < value <= max_val)
+        lower_approaching = min_val is not None and min_val <= value < min_val + tolerance
+        upper_approaching = max_val - tolerance < value <= max_val
+        return lower_approaching or upper_approaching
 
     def calculate_tolerance(self, value):
         return value * 0.05
 
-    def check_temp_limit(self, temperature):
-        min_temp = 0
-        max_temp = 45
-        tolerance = self.calculate_tolerance(max_temp)
-        
-        if not self.is_within_range(temperature, min_temp, max_temp):
-            print('Temperature is out of range!')
-            return False
-        if self.warning_config['temperature'] and self.is_approaching_limit(temperature, min_temp, max_temp, tolerance):
-            print('Warning: Approaching temperature limit')
-        return True
+    def should_warn(self, limit_type, value, min_val, max_val, tolerance):
+        """
+        Determines if a warning should be issued for approaching the limit of a given parameter.
+        """
+        return self.warning_config[limit_type] and self.is_approaching_limit(value, min_val, max_val, tolerance)
 
-    def check_soc_limit(self, soc):
-        min_soc = 20
-        max_soc = 80
-        tolerance = self.calculate_tolerance(max_soc)
-        
-        if not self.is_within_range(soc, min_soc, max_soc):
-            print('SOC is out of range!')
+    def check_limits(self, value, min_val, max_val, tolerance, limit_type):
+        if not self.is_within_range(value, min_val, max_val):
+            print(f'{limit_type.capitalize()} is out of range!')
             return False
-        if self.warning_config['soc'] and self.is_approaching_limit(soc, min_soc, max_soc, tolerance):
-            print('Warning: Approaching SOC limit')
-        return True
-
-    def check_charge_rate_limit(self, charge_rate):
-        max_charge_rate = 0.8
-        tolerance = self.calculate_tolerance(max_charge_rate)
-        
-        if charge_rate > max_charge_rate:
-            print('Charge rate is out of range!')
-            return False
-        if self.warning_config['charge_rate'] and charge_rate > (max_charge_rate - tolerance):
-            print('Warning: Approaching charge rate limit')
+        if self.should_warn(limit_type, value, min_val, max_val, tolerance):
+            print(f'Warning: Approaching {limit_type} limit')
         return True
 
     def battery_is_ok(self, temperature, soc, charge_rate):
-        temp_ok = self.check_temp_limit(temperature)
-        soc_ok = self.check_soc_limit(soc)
-        charge_rate_ok = self.check_charge_rate_limit(charge_rate)
+        temp_ok = self.check_limits(temperature, 0, 45, self.calculate_tolerance(45), 'temperature')
+        soc_ok = self.check_limits(soc, 20, 80, self.calculate_tolerance(80), 'soc')
+        charge_rate_ok = self.check_limits(charge_rate, None, 0.8, self.calculate_tolerance(0.8), 'charge_rate')
         return temp_ok and soc_ok and charge_rate_ok
 
     def configure_warnings(self, temperature=True, soc=True, charge_rate=True):
@@ -67,4 +44,4 @@ class BatteryMonitor:
 
 if __name__ == '__main__':
     monitor = BatteryMonitor()
-    monitor.configure_warnings(temperature=False)  # Example configuration to disable temperature warnings
+    monitor.configure_warnings(temperature=False)
