ec59e9dbb20ac116f81b9add5823ad38cbd69a4c
amrutha0404 <99201771+amrutha0404@users.noreply.github.com>
2024-08-01 16:49:43 +0530

    Update check_limits.py

--- check_limits.py
+++ check_limits.py
@@ -1,43 +1,35 @@
+from utils import is_within_range, is_approaching_limit, calculate_tolerance
+
 class BatteryMonitor:
     def __init__(self):
         self.warning_config = {'temperature': True, 'soc': True, 'charge_rate': True}
 
-    def is_within_range(self, value, min_val, max_val):
-        return min_val <= value <= max_val if min_val is not None else value <= max_val
-
-    def is_approaching_limit(self, value, min_val, max_val, tolerance):
-        lower_approaching = min_val is not None and min_val <= value < min_val + tolerance
-        upper_approaching = max_val - tolerance < value <= max_val
-        return lower_approaching or upper_approaching
-
-    def calculate_tolerance(self, value):
-        return value * 0.05
-
     def should_warn(self, limit_type, value, min_val, max_val, tolerance):
-        """
-        Determines if a warning should be issued for approaching the limit of a given parameter.
-        """
-        return self.warning_config[limit_type] and self.is_approaching_limit(value, min_val, max_val, tolerance)
+        return self.warning_config[limit_type] and is_approaching_limit(value, min_val, max_val, tolerance)
 
     def check_limits(self, value, min_val, max_val, tolerance, limit_type):
-        if not self.is_within_range(value, min_val, max_val):
-            print(f'{limit_type.capitalize()} is out of range!')
-            return False
-        if self.should_warn(limit_type, value, min_val, max_val, tolerance):
-            print(f'Warning: Approaching {limit_type} limit')
-        return True
+        value_ok = is_within_range(value, min_val, max_val)
+        warning_needed = self.should_warn(limit_type, value, min_val, max_val, tolerance)
+        return value_ok, warning_needed
+
+    def handle_warnings(self, is_parameter_within_limits, is_approaching_limit_alert, parameter_name):
+        if is_approaching_limit_alert:
+            print(f'Warning: Approaching {parameter_name} limit')
+        elif not is_parameter_within_limits:
+            print(f'{parameter_name} is out of range!')
 
     def battery_is_ok(self, temperature, soc, charge_rate):
-        temp_ok = self.check_limits(temperature, 0, 45, self.calculate_tolerance(45), 'temperature')
-        soc_ok = self.check_limits(soc, 20, 80, self.calculate_tolerance(80), 'soc')
-        charge_rate_ok = self.check_limits(charge_rate, None, 0.8, self.calculate_tolerance(0.8), 'charge_rate')
+        temp_ok, temp_warning = self.check_limits(temperature, 0, 45, calculate_tolerance(45), 'temperature')
+        soc_ok, soc_warning = self.check_limits(soc, 20, 80, calculate_tolerance(80), 'soc')
+        charge_rate_ok, charge_rate_warning = self.check_limits(charge_rate, None, 0.8, calculate_tolerance(0.8), 'charge_rate')
+
+        self.handle_warnings(temp_ok, temp_warning, 'Temperature')
+        self.handle_warnings(soc_ok, soc_warning, 'State of Charge (SOC)')
+        self.handle_warnings(charge_rate_ok, charge_rate_warning, 'Charge rate')
+
         return temp_ok and soc_ok and charge_rate_ok
 
     def configure_warnings(self, temperature=True, soc=True, charge_rate=True):
-        """
-        Configures the warning settings for the battery monitor system. 
-        This function allows the user to enable or disable warnings for temperature, state of charge (SOC), and charge rate based on the parameters passed.
-        """
         self.warning_config['temperature'] = temperature
         self.warning_config['soc'] = soc
         self.warning_config['charge_rate'] = charge_rate
